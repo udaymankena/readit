035228406c8e3ae6b23271de8cb5b4eb40b08b57
udaymankena <udaymankena91@gmail.com>
2016-08-21 21:08:38 -0700

    added list_authors methoda and overwrote save method

--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
 
 class Book(models.Model):
@@ -10,7 +10,18 @@ class Book(models.Model):
     is_favourite = models.BooleanField(default=False, verbose_name="Favourite?")
 
     def __str__(self):
-        return self.title
+        return "{} by {}".format(self.title, self.list_authors())
+
+    def list_authors(self):
+        return ",".join(author.name for author in Author.objects.all())
+
+    def save(self, *args, **kwargs): # overwritng the save method to suit our needs
+        # this save adds a review date, if the user doesn't mention one
+        if( self.review and self.date_reviewed is None):
+            self.date_reviewed = now()
+
+        super(Book, self).save(*args, **kwargs) # calling the actual overwritten save method, to save the
+                                                # object ot the database
 
 
 class Author(models.Model):
